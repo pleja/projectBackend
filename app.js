d8186c2e6246a0f353f475a609217901019dcb28
Pawel Leja <pleja918@gmail.com>
2019-09-27 20:17:08 -0400

    0.2 Pulls simple API call and transmits to front

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require('express')
 const cors = require ('cors')
+const axios = require('axios')
 const app = express()
 const port = 3001
 
 app.use(cors())
-
+let dude = "hellow"
 app.get('/hi', (req, res) => {
-
-    res.send("hello")
-    console.log(res);
+    let message;
+    axios({
+        method: 'get',
+        url: 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/map',
+        params: {
+            'symbol': 'BTC,USDT,BNB'
+        },
+        headers: {
+            'X-CMC_PRO_API_KEY': '1277fdcd-7db4-4d4b-baa1-5cafe999d92f',
+            'cache-control': 'no-cache'
+        },
+        responseType: 'json'
+    }).then(result => {
+        let quotes = result.data.data
+        JSON.stringify(quotes)
+        console.log(quotes)
+        message = quotes
+        res.send(message)
+        
+    });
+    console.log(message)
+    console.log(dude)
+    
+    //console.log(res);
 })
 
 
